94fdd55db842581ad6ac11ecd68ee3474b8ee577
ElderEt <ElderEt@ENG121-19>
2025-09-22 14:18:16 -0500

    Add project files.

---
+++ TestAppCS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to CS class to explore VS");
+            Console.ReadKey();
+        }
+    }
+}
